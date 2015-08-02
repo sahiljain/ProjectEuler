ae2b0104424de2e4efb86e1d5912225d2ad4755e
Sahil Jain <sahil.jain@live.ca>
2015-08-01 21:42:36 -0400

    update problem 79

--- prob79.scala
+++ prob79.scala
@@ -1,7 +1,6 @@
 object prob79 {
   def main(args: Array[String]): Unit = {
     val keys:List[Int] = scala.io.Source.fromFile("keylog.txt").getLines().map(_.toInt).toList
-    println(keys)
     for (i <- 0 to 999999999) {
       if (keys.forall(isValid(_, i))) {
         println(i)
@@ -14,8 +13,8 @@ object prob79 {
     val keyStr = ""+key
     val sourceStr = ""+source
     val midChar = sourceStr.indexOf(keyStr.charAt(1))
-    val leftChar: Int = sourceStr.indexOf(keyStr.charAt(0))
-    val rightChar: Int = sourceStr.indexOf(keyStr.charAt(2))
+    val leftChar = sourceStr.indexOf(keyStr.charAt(0))
+    val rightChar = sourceStr.indexOf(keyStr.charAt(2))
     leftChar >= 0 && leftChar < midChar && midChar < rightChar
   }
 }
