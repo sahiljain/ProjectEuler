eb46762c01d7b2a308a6938c76982b38dc160c03
Sahil Jain <sahil.jain@live.ca>
2015-08-01 21:32:23 -0400

    finish problem 79

---
+++ prob79.scala
@@ -0,0 +1,21 @@
+object prob79 {
+  def main(args: Array[String]): Unit = {
+    val keys:List[Int] = scala.io.Source.fromFile("keylog.txt").getLines().map(_.toInt).toList
+    println(keys)
+    for (i <- 0 to 999999999) {
+      if (keys.forall(isValid(_, i))) {
+        println(i)
+        return
+      }
+    }
+  }
+
+  def isValid(key: Int, source: Int) : Boolean = {
+    val keyStr = ""+key
+    val sourceStr = ""+source
+    val midChar = sourceStr.indexOf(keyStr.charAt(1))
+    val leftChar: Int = sourceStr.indexOf(keyStr.charAt(0))
+    val rightChar: Int = sourceStr.indexOf(keyStr.charAt(2))
+    leftChar >= 0 && leftChar < midChar && midChar < rightChar
+  }
+}
